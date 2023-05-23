d4287306d41930ced270720525ea64e1a2f2f797
Raghuram Elancheran <83778193+raghurame@users.noreply.github.com>
2023-05-23 16:52:01 +0530

    first commit

---
+++ dumpAverages.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+int findNAtoms (int nAtoms, FILE *inputDump)
+{
+	char lineString[2000];
+
+	for (int i = 0; i < 4; ++i)
+	{
+		fgets (lineString, 2000, inputDump);
+	}
+
+	sscanf (lineString, "%d\n", &nAtoms);
+
+	rewind (inputDump);
+	return nAtoms;
+}
+
+/*int stringCount (int nNeedles, char haystack[], char needle[])
+{
+	char *tempString = needle;
+	nNeedles = 0;
+
+	printf("Initial value for tempString\n");
+	printf("'%s'\n", tempString);
+
+	while (*tempString != '\0' && (tempString = strstr (haystack, tempString)))
+	{
+		nNeedles++;
+		++tempString;
+		printf("%s\n", tempString);
+		sleep (1);
+	}
+
+	return nNeedles;
+}
+*/
+
+int countSpaces (int nColumns, char lineString[])
+{
+	int stringLength = strlen (lineString);
+	nColumns = 0;
+
+	for (int i = 0; i < stringLength; ++i)
+	{
+		if (lineString[i] == ' ')
+		{
+			nColumns++;
+		}
+	}
+
+	return nColumns;
+}
+
+int findNColumns (int nColumns, FILE *inputDump)
+{
+	char lineString[2000];
+
+	/*snprintf (space, 10, " ");*/
+
+	for (int i = 0; i < 10; ++i)
+	{
+		fgets (lineString, 2000, inputDump);
+	}
+
+	/*nColumns = stringCount (nColumns, lineString, space);*/
+	nColumns = countSpaces (nColumns, lineString);
+
+	rewind (inputDump);
+	return nColumns;
+}
+
+float ***readDumpfile (float ***dumpValues, int nColumns, int nAtoms, FILE *inputDump, int *nTimeframes)
+{
+	char lineString[2000];
+	(*nTimeframes) = 0;
+
+	int fileStatus = fgetc (inputDump);
+
+	while (fileStatus != EOF)
+	{
+		for (int i = 0; i < 9; ++i) {
+			fgets (lineString, 2000, inputDump); }
+
+		for (int i = 0; i < nAtoms; ++i)
+		{
+			for (int j = 0; j < nColumns; ++j) {
+				fscanf (inputDump, "%f ", &dumpValues[(*nTimeframes)][j][i]); }
+
+			fscanf (inputDump, "\n");
+		}
+
+		(*nTimeframes)++;
+		fileStatus = fgetc (inputDump);
+	}
+
+	return dumpValues;
+}
+
+int countNTimesteps (int nTimesteps, FILE *inputDump, int nAtoms, const char inputFilename[])
+{
+	FILE *pipeLineCount;
+	char *pipeString, lineString[2000];
+
+	pipeString = (char *) malloc (100 * sizeof (char));
+	snprintf (pipeString, 100, "wc -l %s", inputFilename);
+
+	pipeLineCount = popen (pipeString, "r");
+	fgets (lineString, 2000, pipeLineCount);
+	sscanf (lineString, "%d", &nTimesteps);
+
+	return nTimesteps;
+}
+
+int main(int argc, char const *argv[])
+{
+	FILE *inputDump;
+	inputDump = fopen (argv[1], "r");
+
+	int nAtoms = findNAtoms (nAtoms, inputDump), nColumns = findNColumns (nColumns, inputDump), nTimeframes = 0, nTimesteps = countNTimesteps (nTimesteps, inputDump, nAtoms, argv[1]);
+
+	float ***dumpValues;
+	dumpValues = (float ***) malloc (nTimesteps * sizeof (float **));
+
+	for (int i = 0; i < nTimesteps; ++i) {
+		dumpValues[i] = (float **) malloc (nColumns * sizeof (float *)); }
+
+	for (int i = 0; i < nTimesteps; ++i)
+	{
+		for (int j = 0; j < nColumns; ++j) {
+			dumpValues[i][j] = (float *) malloc (nAtoms * sizeof (float)); }
+	}
+
+	dumpValues = readDumpfile (dumpValues, nColumns, nAtoms, inputDump, &nTimeframes);
+
+	for (int i = 0; i < nAtoms; ++i)
+	{
+		printf("%f\n", dumpValues[0][3][i]);
+		usleep (100000);
+	}
+
+/*	float *ensembleAvg, *timeAvg;
+	ensembleAvg = (float *) malloc (nTimeframes * sizeof (float));
+	timeAvg = (float *) malloc (nAtoms * sizeof (float));
+
+	ensembleAvg = computeEnsembleAvg (ensembleAvg, nTimeframes, dumpValues, nColumns, nAtoms);
+	timeAvg = computeTimeAvg (timeAvg, )
+*/
+	fclose (inputDump);
+	return 0;
+}
